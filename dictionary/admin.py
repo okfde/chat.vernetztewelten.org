23039bac277ffd856fd5c4ca49ed268c49c1f6c5
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-02-05 11:44:44 +0100

    Add basic admin config

--- dictionary/admin.py
+++ dictionary/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Dictionary
+
+
+class DictionaryAdmin(admin.ModelAdmin):
+    raw_id_fields = ('room',)
+
+
+admin.register(Dictionary, DictionaryAdmin)
